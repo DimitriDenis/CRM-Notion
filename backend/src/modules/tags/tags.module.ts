ddcd33d94979c4e1394bbc0cd5428681c8415bbc
dimitri <dimitri.dd@outlook.fr>
2025-01-17 11:20:19 +0100

    feat(backend): Add tag module

---
+++ backend/src/modules/tags/tags.module.ts
@@ -0,0 +1,14 @@
+// src/modules/tags/tags.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TagsController } from './tags.controller';
+import { TagsService } from './tags.service';
+import { Tag } from './tag.entity';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Tag])],
+  controllers: [TagsController],
+  providers: [TagsService],
+  exports: [TagsService],
+})
+export class TagsModule {}
