195f6b7435d089fb5a116f3aef1f8e3e4b88b74a
dimitri <dimitri.dd@outlook.fr>
2025-01-22 22:01:38 +0100

    feat(backend): Add CORS config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,6 +3,17 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  // Configuration CORS
+  app.enableCors({
+    origin: 'http://localhost:3000', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: ['Content-Type', 'Accept', 'Authorization'],
+    credentials: true,
+  });
+
+
   await app.listen(3001);
 }
 bootstrap();
