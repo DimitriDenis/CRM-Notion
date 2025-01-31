198ec7b3e432dde69b1f62114ae7f5fd77a5f1e0
dimitri <dimitri.dd@outlook.fr>
2025-01-31 13:21:45 +0100

    chore(frontend): Debug redirection to dashboard

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('token');
+  const token = request.cookies.get('auth-token');
 
   if (request.nextUrl.pathname.startsWith('/auth/') && token) {
     return NextResponse.redirect(new URL('/dashboard', request.url));
