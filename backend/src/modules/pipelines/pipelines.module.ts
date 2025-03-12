4c87e191ac5d0dfcd748e1eb4f48093607e3687f
dimitri <dimitri.dd@outlook.fr>
2025-03-12 14:21:27 +0100

    feat(frontend): Add Deal on pipeline module

--- backend/src/modules/pipelines/pipelines.module.ts
+++ backend/src/modules/pipelines/pipelines.module.ts
@@ -4,9 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { PipelinesController } from './pipelines.controller';
 import { PipelinesService } from './pipelines.service';
 import { Pipeline } from './pipeline.entity';
+import { Deal } from '../deals/deal.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Pipeline])],
+  imports: [TypeOrmModule.forFeature([Pipeline]), TypeOrmModule.forFeature([Deal]),],
   controllers: [PipelinesController],
   providers: [PipelinesService],
   exports: [PipelinesService],
