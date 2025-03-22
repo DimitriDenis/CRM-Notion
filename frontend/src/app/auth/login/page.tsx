8b92100e54eb4dd8e4ea866bdc4e2d30644305e4
dimitri <dimitri.dd@outlook.fr>
2025-03-22 10:26:48 +0100

    feat(frontend): suspense boundary fo auth login

--- frontend/src/app/auth/login/page.tsx
+++ frontend/src/app/auth/login/page.tsx
@@ -1,11 +1,15 @@
 // src/app/auth/login/page.tsx
 import LoginForm from '@/components/auth/LoginForm';
 import AuthLayout from '@/components/auth/AuthLayout';
+import { Suspense } from 'react';
+import LoginClient from '@/components/client-wrapper/LoginClient';
 
 export default function LoginPage() {
   return (
     <AuthLayout>
-      <LoginForm />
+       <Suspense fallback={<div>Chargement...</div>}>
+            <LoginClient />
+          </Suspense>
     </AuthLayout>
   );
 }
---
+++ frontend/src/components/client-wrapper/LoginClient.tsx
@@ -0,0 +1,8 @@
+// src/components/client-wrappers/LoginClient.tsx
+'use client';
+
+import LoginForm from '@/components/auth/LoginForm';
+
+export default function LoginClient() {
+  return <LoginForm />;
+}
