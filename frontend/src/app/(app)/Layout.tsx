4ae3724e4f0a0f04196da2c5d71bfa2ea4d913da
dimitri <dimitri.dd@outlook.fr>
2025-03-22 13:36:49 +0100

    feat(frontend): debug layout

--- frontend/src/app/(app)/Layout.tsx
+++ frontend/src/app/(app)/Layout.tsx
@@ -1,9 +1,7 @@
 // src/app/(app)/layout.tsx
 import { ReactNode } from 'react';
-import ClientLayoutWrapper from '@/components/client-wrapper/ClientLayoutWrapper';
 
 export default function AppLayout({ children }: { children: ReactNode }) {
-  console.log("AppLayout rendering (server component)");
-  return <ClientLayoutWrapper>{children}</ClientLayoutWrapper>;
+  return children;
 }
 
