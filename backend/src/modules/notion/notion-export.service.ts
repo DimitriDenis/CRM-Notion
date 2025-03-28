6b914b40e6c27d8760c710b5030f70a42628be9a
dimitri <dimitri.dd@outlook.fr>
2025-03-28 11:17:57 +0100

    chore(backend): maintenance

--- backend/src/modules/notion/notion-export.service.ts
+++ backend/src/modules/notion/notion-export.service.ts
@@ -33,7 +33,7 @@ export class NotionExportService {
     }
 
     const notion = this.getNotionClient(user.notionAccessToken);
-    let workspaceId = options.workspaceId || user.notionWorkspaceId;
+    let workspaceId = user.notionWorkspaceId;
     
     const isValidUUID = (id) => {
         return id && /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(id);
