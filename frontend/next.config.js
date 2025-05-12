55c1c389497db0662c2e4c40564fabaeb29693fb
dimitri <dimitri.dd@outlook.fr>
2025-05-12 18:14:15 +0200

    feat(frontend): debug

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-const withNextIntl = require('next-intl/plugin')();
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // ... autres configurations existantes
-};
-
-module.exports = withNextIntl(nextConfig); 
