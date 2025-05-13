9cac5f92c76d77b3aac3a595c5b102b25f0d83b4
dimitri <dimitri.dd@outlook.fr>
2025-05-13 11:19:02 +0200

    feat(frontend): debug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,8 +1,10 @@
-const withNextIntl = require('next-intl/plugin')();
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // ... autres configurations existantes
 };
 
-module.exports = withNextIntl(nextConfig); 
+module.exports = withNextIntl(nextConfig);
