9c0f01c27b7e5f5f839c3eab348b084040d35e7b
dimitri <dimitri.dd@outlook.fr>
2025-05-13 11:33:50 +0200

    feat(frontend): debug

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,6 @@
 const createNextIntlPlugin = require('next-intl/plugin');
 
-const withNextIntl = createNextIntlPlugin('./i18n.ts'); // Notez l'extension .ts ici
+const withNextIntl = createNextIntlPlugin('./src/i18n.ts'); // Notez l'extension .ts ici
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
